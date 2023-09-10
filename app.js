ac3a36b359167af1cadc547315f0780524ee7ee8
francoromeo <francoromeo5012@gmail.com>
2023-09-09 23:48:59 -0300

    js and css medias

--- app.js
+++ app.js
@@ -19,3 +19,12 @@ function changeSearchIcon(){
     }
 }
 changeSearchIcon();
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
